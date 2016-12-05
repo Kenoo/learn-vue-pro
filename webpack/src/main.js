134360855d242eb82f0f4226f93306d27dae9154
kenoo <312008981@qq.com>
2016-12-05 08:20:11 +0800

    add webpack demo

---
+++ webpack/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import routers from './routers'
+
+new Vue({
+  el: '#app',
+  data : {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  render(h) {
+    return h(this.ViewComponent)
+  }
+
+})
