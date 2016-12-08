9cab1984dc9291f137e0bb9a176d521483eff611
kenoo <312008981@qq.com>
2016-12-09 00:21:50 +0800

    add vue-markdown plugins,

--- webpack/src/pages/Home.vue
+++ webpack/src/pages/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <main-layout>
     <p>Welcome home</p>
+    <markdown></markdown>
   </main-layout>
 </template>
 
 <script>
   import MainLayout from '../layouts/Main.vue'
+  import Markdown from '../examples/test.md'
+
   export default {
     components: {
-      MainLayout
+      MainLayout,
+      Markdown
     }
   }
 </script>
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,72 @@
 var path = require('path')
 var webpack = require('webpack')
+var slugify = require('transliteration').slugify
+var striptags = require('./strip-tags');
 
 var markdown = require('markdown-it')({
   html: true,
-  breaks: true
+  breaks: true,
+  preprocess: function(MarkdownIt, source) {
+    MarkdownIt.renderer.rules.table_open = function() {
+      return '<table class="table">';
+    };
+    MarkdownIt.renderer.rules.fence = wrap(MarkdownIt.renderer.rules.fence);
+    return source;
+  }
 })
 
-// markdown
-//   .use(plugin1)
-//   .use(plugin2, opts, ...)
-//   .use(plugin3);
+var wrap = function(render) {
+  return function() {
+    return render.apply(this, arguments)
+      .replace('<code class="', '<code class="hljs ')
+      .replace('<code>', '<code class="hljs">');
+  };
+}
+
+function convert(str) {
+  str = str.replace(/(&#x)(\w{4});/gi, function($0) {
+    return String.fromCharCode(parseInt(encodeURIComponent($0).replace(/(%26%23x)(\w{4})(%3B)/g, '$2'), 16));
+  });
+  return str;
+}
+
+
+markdown.use(
+    require('markdown-it-anchor'),{
+    level: 2,
+    slugify: slugify,
+    permalink: true,
+    permalinkBefore: true
+  }).use(require('markdown-it-container'),'demo', {
+    validate: function(params) {
+      return params.trim().match(/^demo\s*(.*)$/);
+    },
+
+    render: function(tokens, idx) {
+      var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
+      if (tokens[idx].nesting === 1) {
+        var description = (m && m.length > 1) ? m[1] : '';
+        var content = tokens[idx + 1].content;
+        var html = convert(striptags.strip(content, ['script', 'style'])).replace(/(<[^>]*)=""(?=.*>)/g, '$1');
+        var script = striptags.fetch(content, 'script');
+        var style = striptags.fetch(content, 'style');
+        var jsfiddle = { html: html, script: script, style: style };
+        var descriptionHTML = description
+          ? markdown.render(description)
+          : '';
+
+        jsfiddle = markdown.utils.escapeHtml(JSON.stringify(jsfiddle));
+
+        return `<demo-block class="demo-box" :jsfiddle="${jsfiddle}">
+                  <div class="source" slot="source">${html}</div>
+                  ${descriptionHTML}
+                  <div class="highlight" slot="highlight">`;
+      }
+      return '</div></demo-block>\n';
+    }
+  })
+
+
 module.exports = {
   entry: './src/main.js',
   output: {
