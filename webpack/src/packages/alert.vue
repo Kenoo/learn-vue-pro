d93d8e29fad2788f574f1e14c097eec3001081fd
kenoo <312008981@qq.com>
2016-12-10 00:48:22 +0800

    add closed method to sf-alert

--- webpack/src/packages/alert.vue
+++ webpack/src/packages/alert.vue
@@ -1,10 +1,18 @@
 <template>
-  <p>alert</p>
+  <div class="sf-alert">
+    <p>alert</p>
+    <p v-on:click="doClick">close</p>
+  </div>
 </template>
 <script>
 export default {
   data () {
     return {};
+  },
+  methods: {
+    doClick: function () {
+      this.$emit("close");
+    }
   }
 }
 </script>
